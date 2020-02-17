7776a43245c0639019759bfc3646cbd36e2f85e0
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2020-02-17 12:19:48 +0300

    Первый внешний вид карточек

--- server.js
+++ server.js
@@ -4,11 +4,33 @@ const app = express();
 
 nunjucks.configure('views', {
     autoescape: true,
-    express: app
+    express: app,
+    noCache: true
 });
 
+const content = "Привет, это первое объяснение, которое уходит на много буков. Привет, это первое объяснение, которое уходит на много буков. Привет, это первое объяснение, которое уходит на много буков. Привет, это первое объяснение, которое уходит на много буков.";
+const cards = [
+    {
+        title: "Слово 1", 
+        content: content, 
+        sources: [
+            {author: "Танаев В.В.", title: "Книга Влада", link: "стр. 24 строка 20"},
+            {author: "Зубенко М.П.", title: "Книга Пэтровича", link: "стр. 1337 строка 420"}
+        ]
+    },
+    {
+        title: "Слово 2", 
+        subtitle: "В значении 'Кек'", 
+        content: content,
+        sources: [
+            {author: "Танаев В.В.", title: "Книга Влада #2", link: "стр. 42 строка 2"},
+            {author: "Зубенко М.П.", title: "Книга Пэтровича", link: "стр. 420 строка 12"}
+        ]
+    }
+];
+
 app.get('/', (req, res) => {
-    res.render('test.html', {name: "Vasya"});
+    res.render('viewer.html', { cards });
 });
 
 app.use(express.static('public'));
